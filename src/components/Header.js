b748e13cec53f92f6f5473f81f10c33f2700f59d
= <=>
2020-07-21 20:38:41 +0200

    Add Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import css from './../css/header.css';
+
+const Header = props => {
+    return (
+        <header>
+            <h1>
+                <span className="box">Box</span> Shadow Generator
+            </h1>
+        </header>
+    )
+};
+
+export default Header;
