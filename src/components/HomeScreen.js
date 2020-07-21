b3800ef5f17d7ff03983ca3bb5964ce6c2bcae11
= <=>
2020-07-21 20:38:49 +0200

    Add HomeScreen.js

---
+++ src/components/HomeScreen.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import Slider from './Slider';
+import Header from './Header';
+import Content from './Content';
+import Sliders from './Sliders';
+import Box from './Box';
+import Code from './Code';
+import css from './../css/homescreen.css';
+
+class HomeScreen extends Component {
+
+    render() {
+        return(
+            <div className="container">
+                <Header/>
+                <Content>
+                    <Sliders/>
+                    <Box/>
+                </Content>
+                <Content>
+                    <Code/>
+                </Content>
+            </div>
+        )
+    }
+};
+
+export default HomeScreen;
