d167641a815eea44d875dd09e609d6d22d5aa0b3
= <=>
2020-07-21 20:38:08 +0200

    Add Box.js

---
+++ src/components/Box.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import css from './../css/box.css';
+
+const Box = props => {
+
+    return(
+        <div className="shadow-box-container">
+            <div className="shadow-box">
+                
+            </div>
+        </div>
+    )
+};
+
+export default Box;
