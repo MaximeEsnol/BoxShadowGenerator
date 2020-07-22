398517946c3f4613c209175b40a2a89e65fae0cd
MaximeEsnol <maxime.esnol02@gmail.com>
2020-07-22 19:36:32 +0200

    Add box shadow value

--- src/components/Code.js
+++ src/components/Code.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Code = props => {
     return(
         <div className="code">
-
+            <code>
+                box-shadow: {props.shadows};
+            </code>
         </div>
     )
 };
