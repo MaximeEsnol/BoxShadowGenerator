57c1a993b4a5afd35b2251aa366b4b5b1fb442c9
MaximeEsnol <maxime.esnol02@gmail.com>
2020-07-22 17:48:27 +0200

    Add box-shadow styles

--- src/components/Box.js
+++ src/components/Box.js
@@ -5,7 +5,7 @@ const Box = props => {
 
     return(
         <div className="shadow-box-container">
-            <div className="shadow-box">
+            <div className="shadow-box" style={{boxShadow: props.boxShadow}}>
                 
             </div>
         </div>
