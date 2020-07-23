2741c6a4800c1a77481c532879f757f4eb5a642c
MaximeEsnol <maxime.esnol02@gmail.com>
2020-07-23 15:03:35 +0200

    FIX: ShadowLayers not showing

--- src/components/ShadowList.js
+++ src/components/ShadowList.js
@@ -2,12 +2,11 @@ import React from 'react';
 import ShadowLayer from './ShadowLayer';
 
 const ShadowList = props => {
-
     return (
         <div className="shadow-list">
             {
-                props.shadows.forEach( ( shadow, index ) => 
-                    <ShadowLayer order={index} shadow={shadow}/>
+                props.shadows.map( ( shadow, index ) => 
+                    <ShadowLayer key={index} isActive={(props.current === index)} order={index} shadow={shadow}/>
                 )
             }
         </div>
