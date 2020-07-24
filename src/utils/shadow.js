2ec3499b614c163bd0a65ce0851a46dc5ddbfec3
MaximeEsnol <maxime.esnol02@gmail.com>
2020-07-24 14:56:30 +0200

    Add stringToRgbObject() function

--- src/utils/shadow.js
+++ src/utils/shadow.js
@@ -14,6 +14,22 @@ export const getBoxShadowFromObject = shadows => {
     return boxShadow;
 }
 
+export const stringToRgbObject = rgb => {
+    let colors = rgb.substring( rgb.indexOf("(") + 1, rgb.length - 1 );
+    let color = colors.split(",");
+
+    if ( color.length < 3 ) {
+        throw new Error("Invalid RGB string.");
+    }
+
+    return {
+        r: color[0],
+        g: color[1],
+        b: color[2],
+        a: color[3] ?? 1
+    }
+}
+
 export const DEFAULT_SHADOW = {
     inset: false,
     offsetX: 0,
