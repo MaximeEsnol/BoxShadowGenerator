4c75918f12bac8d2acfc0dd53f6694d36ee86822
MaximeEsnol <maxime.esnol02@gmail.com>
2020-07-25 16:16:00 +0200

    Added open/close icon

--- src/components/Code.js
+++ src/components/Code.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { getBoxShadowFromObject } from './../utils/shadow';
 import './../css/code.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronUp } from '@fortawesome/free-solid-svg-icons';
 
 const Code = props => {
 
     return (
         <div className="code">
+            <FontAwesomeIcon icon={faChevronUp}/>
             <code>
                 box-shadow: { getBoxShadowFromObject( props.shadows ) };
             </code>
