eb551f1b75a36dce7b5cfbd993f02f26ad418410
maximeesnol <maxime.esnol02@gmail.com>
2022-10-02 10:44:20 +0200

    Update build

---
+++ precache-manifest.a4144797a3bf83ae00e73efec7cc07f6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da49ef1b6bdf2d7b8e6bd888fe96ba94",
+    "url": "/BoxShadowGenerator/index.html"
+  },
+  {
+    "revision": "9b8fb3ee94966bf2b310",
+    "url": "/BoxShadowGenerator/static/css/main.da43d4e3.chunk.css"
+  },
+  {
+    "revision": "a326135da5806fee4173",
+    "url": "/BoxShadowGenerator/static/js/2.710a7ba4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BoxShadowGenerator/static/js/2.710a7ba4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b8fb3ee94966bf2b310",
+    "url": "/BoxShadowGenerator/static/js/main.a99abd09.chunk.js"
+  },
+  {
+    "revision": "8df65ad965f0efff113c",
+    "url": "/BoxShadowGenerator/static/js/runtime-main.e2a2185d.js"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/BoxShadowGenerator/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "88823c2015ffd5fa89d567e17297a137",
+    "url": "/BoxShadowGenerator/static/media/Roboto-Light.88823c20.ttf"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "/BoxShadowGenerator/static/media/Roboto-Medium.58aef543.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/BoxShadowGenerator/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/BoxShadowGenerator/static/media/logo.5d5d9eef.svg"
+  }
+]);
